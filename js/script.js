e49179c96ec6cbe6f1324e9bb97024616c33f710
sannasinne <sanna@keistinen.fi>
2024-01-23 10:33:33 +0200

    Change event added

---
+++ js/script.js
@@ -0,0 +1,19 @@
+var body = document.querySelector("body");
+var moods = document.querySelector("#moods");
+
+moods.addEventListener("change", function (e) {
+  var mood = e.target.value;
+  if (mood === "happy") {
+    body.classList.remove("sad");
+    body.classList.remove("passionate");
+    body.classList.add("happy");
+  } else if (mood === "sad") {
+    body.classList.remove("happy");
+    body.classList.remove("passionate");
+    body.classList.add("sad");
+  } else if (mood === "passionate") {
+    body.classList.remove("happy");
+    body.classList.remove("sad");
+    body.classList.add("passionate");
+  }
+});
